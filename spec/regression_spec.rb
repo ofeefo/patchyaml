788cb5ab17acfa39c0fd695f74f43468db9ebd11
Felipe Mariotti <felipe.mtt95@gmail.com>
2025-11-26 19:19:31 -0300

    feat: Add end_offset overflow test case

--- spec/regression_spec.rb
+++ spec/regression_spec.rb
@@ -33,4 +33,25 @@ RSpec.describe PatchYAML do
                   - '2'
     YAML
   end
+
+  it "does not overflows on end_offset" do
+    data = value.strip
+    bump_path = "some-app.staticDeploy.buckets.some-staging.invalidations.SOMESTRANGEID"
+
+    editor = PatchYAML.load(data)
+    editor.update(bump_path, ["3", "4"])
+    expect { data = editor.yaml }.to_not raise_error
+    expect(data).to eq(<<~YAML)
+      some-app:
+        staticDeploy:
+          enabled: true
+          buckets:
+            some-staging:
+              version: 0.11.22
+              invalidations:
+                SOMESTRANGEID:
+                  - '3'
+                  - '4'
+    YAML
+  end
 end
